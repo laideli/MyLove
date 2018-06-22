6bffb7eb93239327b12e45450187bf7503080356
wangfang <1102666413@qq.com>
2018-06-22 15:47:10 +0800

    增加远程读取配置

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/wangfang/springcloud/DeptProvider8001_ConfigApp.java
@@ -0,0 +1,15 @@
+package com.wangfang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class DeptProvider8001_ConfigApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8001_ConfigApp.class,args);
+    }
+}
---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/wangfang/springcloud/dao/DeptDao.java
@@ -0,0 +1,16 @@
+package com.wangfang.springcloud.dao;
+
+import com.wangfang.springcloud.entities.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DeptDao {
+
+    public boolean addDept(Dept dept);
+
+    public Dept findById(Long id);
+
+    public List<Dept> findAll();
+}
